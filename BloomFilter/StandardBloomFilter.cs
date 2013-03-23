f46f501f38bd9f2ee0ddc752217e68efec13e6f7
nvanderpyle <NickVanderPyle@users.noreply.github.com>
2013-03-23 16:29:54 -0500

    adding "this"

--- BloomFilter/StandardBloomFilter.cs
+++ BloomFilter/StandardBloomFilter.cs
@@ -14,15 +14,15 @@ namespace BloomFilter
 		{
 			this._filterBits = filterStorage;
 			this._hashGenerator = hashGenerator;
-			_numberOfHashes = GetOptimalNumberOfHashes(estimatedNumberOfElements, filterStorage.Size);
+			this._numberOfHashes = GetOptimalNumberOfHashes(estimatedNumberOfElements, filterStorage.Size);
 		}
 
         public void Add(byte[] item)
         {
-            for (UInt32 i = 0; i < _numberOfHashes; ++i)
+			for (UInt32 i = 0; i < this._numberOfHashes; ++i)
             {
-				var index = _hashGenerator.GetHashCode(item, i);
-                _filterBits[index] = true;
+				var index = this._hashGenerator.GetHashCode(item, i);
+				this._filterBits[index] = true;
             }
         }
 
@@ -30,29 +30,14 @@ namespace BloomFilter
         {
             for (UInt32 i = 0; i < _numberOfHashes; ++i)
             {
-				var index = _hashGenerator.GetHashCode(item, i);
-                if (!_filterBits[index])
+				var index = this._hashGenerator.GetHashCode(item, i);
+				if (!this._filterBits[index])
                 {
                     return false;
                 }
             }
 
             return true;
-        }
-
-        private int GetHashCode(byte[] item, int offset)
-        {
-            //var hashCode = item.GetHashCode();
-            //var mm3Hash = new Murmur3_x64((uint)offset);
-            //mm3Hash.ComputeHash(item);
-            //var lowerHash = (long)mm3Hash.HashLowerBound;
-            //var upperHash = (long)mm3Hash.HashUpperBound;
-
-			var hash = new Murmurhash32().GetHashCode(item, (uint)offset);
-
-            var result = (int)(hash + (offset * hash)) % _filterBits.Size;
-
-            return Math.Abs(result);
         }
 
         public byte[] GetBloomFilterBytes()
